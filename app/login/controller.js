aa05a0961f880721284f4704de8f34d89b47d586
William Pacetti <wspacetti@gmail.com>
2017-04-06 15:23:45 -0500

    Error messages and controller

--- app/login/controller.js
+++ app/login/controller.js
@@ -1,4 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  formValues: {
+    email: '',
+    password: ''
+  },
+
+  actions: {
+    registerUser() {
+      // Why this.store.createCrecord?
+      const user = this.store.createRecord('user', this.formValues);
+
+      // line 15 is what sends the data to back end
+      user.save()
+        // then when it comes back
+        .then(() => {
+          // Sets formValues object to an empty object
+          this.set('formValues', {});
+
+          // @TODO: Change where this routes after login
+          this.transitionToRoute('index');
+        }, (err) => {
+          this.set('error', 'Wrong password, try again!');
+          debugger;
+        });
+    }
+  }
 });
