f0e801e806e1152740c0acc5f73b932196b52d49
Kelly Clemons <kellyclemons@Kellys-MacBook-Air.local>
2017-04-10 14:54:04 -0500

    changes to the create2 controller

--- app/create2/controller.js
+++ app/create2/controller.js
@@ -10,5 +10,17 @@ export default Ember.Controller.extend({
       'Thursday',
       'Friday',
       'Saturday'
-    ]
+    ],
+
+    choices: [],
+
+    actions: {
+      chooseAvailability(availability) {
+        if (this.choices.includes(availability)) {
+          alert("you've already chosen this item");
+        } else {
+          this.set('choices', [...this.choices, availability])
+        }
+      }
+    }
 });
