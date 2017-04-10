c8498e9e62481a541079d718790e037349aecc92
Kelly Clemons <kellyclemons@Kellys-MacBook-Air.local>
2017-04-10 14:23:07 -0500

    created arrays for profile setup pages

---
+++ app/create2/controller.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  availabilities:
+    [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday'
+    ]
+});
