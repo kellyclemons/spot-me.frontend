407d822676caea5c1555cb013b50b0faaa7f4619
Kelly Clemons <kellyclemons@Kellys-MacBook-Air.local>
2017-04-13 13:34:55 -0500

    gave values to input fields and deleted sidebar

---
+++ app/edit/controller.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    async saveProfile(changeset) {
+      await changeset.validate();
+
+      if (changeset.get('isInvalid')) {
+        return alert('Form data is invalid');
+      }
+
+      changeset.save();
+    }
+  }
+});
---
+++ app/models/edit.js
@@ -0,0 +1,12 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  email: DS.attr('string'),
+  ageRange: DS.attr('string'),
+  gender: DS.attr('string'),
+  zipCode: DS.attr('string'),
+  bio: DS.attr('string'),
+  activities: DS.attr('string'),
+  availability: DS.attr('string'),
+})
