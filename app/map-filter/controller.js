06bfad810ecb01588bbb18cd7d1ac0757273e41d
William Pacetti <wspacetti@gmail.com>
2017-04-13 12:20:53 -0500

    changing map styles and working on ember map addon

---
+++ app/map-filter/controller.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  lat: 32.75494243654723,
+  lng: -86.8359375,
+  zoom: 4
+});
--- app/map-filter/route.js
+++ app/map-filter/route.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  // lat: 32.75494243654723,
+  // lng: -86.8359375,
+  // zoom: 4
 });
