fa95692dced2883d58160f030aa631aa9ca0a9a6
Kelly Clemons <kelly.clemons@mac.com>
2017-04-18 16:44:40 -0500

    Save activities

--- app/profile/route.js
+++ app/profile/route.js
@@ -2,4 +2,9 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  currentUser: Ember.inject.service(),
+
+  model() {
+    return this.get('currentUser').load().catch(() => this.get('session').invalidate());
+  }
 });
---
+++ app/services/current-user.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+const { inject: { service } } = Ember;
+
+export default Ember.Service.extend({
+  session: service('session'),
+  store: service(),
+
+  load() {
+    if (this.get('session.isAuthenticated')) {
+      return this.get('store').queryRecord('user', { current:true }).then((user) => {
+        this.set('user', user);
+
+        return user;
+      });
+    } else {
+      return Ember.RSVP.resolve();
+    }
+  }
+});
